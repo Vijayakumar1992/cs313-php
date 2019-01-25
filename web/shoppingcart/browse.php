53895024ddbe1e9e38fdaaf39a523e0763d77b99
david aruldass <davidaruldass@yahoo.com>
2019-01-25 16:19:05 -0700

    added codes

--- web/shoppingcart/browse.php
+++ web/shoppingcart/browse.php
@@ -1,27 +1,29 @@
 <?php
 
-session-start()
 
 session_start();
 if(!isset($_SESSION['cart'])) {
     $_SESSION['cart'] = array();
     $_SESSION['price'] = array();
 }
+
+
 ?>
 
 
 <!DOCTYPE HTML>
 <html>
 
-
 <head>
     <link rel="stylesheet" href="jersey.css">
 </head>
 
-
+<?php 
+echo $_POST["jersey"];
+?>
 <body>
     <!-- <button id="cartbutton" type="button" onclick="alert('adding to cart!')">Add to Cart</button> -->
-    <h1 id="center">FAVORITE NBA JERSEY'S</h1>
+    <!-- <h1 id="center">FAVORITE NBA JERSEY'S</h1>
     <div class="container">
         <img src="jamesjersey.JPG" width="500px" alt="LeBron James Jersey">
         <img src="kdjersey.png" width="500px" alt="Kevin Durant Jersey">
@@ -30,10 +32,17 @@ if(!isset($_SESSION['cart'])) {
     <div class="buttonflex">
         <button class="cartbutton" type="button" onclick="addToCart('jamesjersey', 99)">Add to Cart</button>
         <button class="cartbutton" type="button" onclick="addToCart('kdjersey', 89)">Add to Cart</button>
-        <button class="cartbutton" type="button" onclick="addToCart('kobebryant', 79)">Add to Cart</button>
-    </div>
+        <button class="cartbutton" type="button" onclick="addToCart('kobebryant', 79)">Add to Cart</button> -->
 
-    <div class="container">
+
+        <img src="jamesjersey.JPG" width="500px" alt="LeBron James Jersey">
+        <form action="#" method="post">
+            <input type="text" hidden name ="jersey" value ="james">
+            <input type="submit" value = "add to card">
+        </form>
+    <!-- </div> -->
+
+    <!-- <div class="container">
         <img src="nashjersey.jpg" width="500px" alt="Steve Nash Jersey">
         <img src="wadejersey.jpg" width="500px" alt="wade  Jersey">
         <img src="westbrookjersey.png" width="500px" alt="westbrook Jersey">
@@ -43,5 +52,5 @@ if(!isset($_SESSION['cart'])) {
         <button class="cartbutton" type="button" onclick="alert('adding to cart!')">Add to Cart</button>
         <button class="cartbutton" type="button" onclick="alert('adding to cart!')">Add to Cart</button>
         <button class="cartbutton" type="button" onclick="alert('adding to cart!')">Add to Cart</button>
-    </div>
+    </div> -->
 </body>
