cc895bbb689132530f4f6f0d5343dd7ddcf45986
david aruldass <davidaruldass@yahoo.com>
2019-01-25 15:32:44 -0700

    updated codes

--- web/shoppingcart/script.js
+++
@@ -1,5 +0,0 @@
-function addToCart(item, price) {
-    var httpRequest = new XMLHttpRequest();
-    httpRequest.open("GET", "cart.php?action=add&item=" + item + "&price=" + price, true);
-    httpRequest.send();
-}
