c3d69637817b1c921a9ab27e2bd925347b9ccdf0
david aruldass <davidaruldass@yahoo.com>
2019-01-25 18:10:26 -0700

    df

--- web/shoppingcart/cart.php
+++ web/shoppingcart/cart.php
@@ -3,44 +3,41 @@
 
 <?php
 session_start();
+
+$removeitem = $_POST["remove"];
+if(removeitem = "jerseyjames"){
+    $_SESSION ['jerseyjames'] = false;
+}
+
 if(isset( $_SESSION["jerseyjames"])) {
     echo "<h1>jersey james printed</h1><br>";
-}else{
-    echo "not in cart";
+
+    <form action="cart.php?remove=jerseyjames">  
+    <input type="submit" value="Submit">
+    </form>
 }
 
 if(isset( $_SESSION["jerseydurant"])) {
     echo "<h1>jersey durant printed</h1><br>";
-}else{
-    echo "not in cart";
 }
 
 
 if(isset( $_SESSION["jerseykobe"])) {
     echo "<h1>jersey kobe printed</h1><br>";
-}else{
-    echo "not in cart";
 }
 
 if(isset( $_SESSION["jerseynash"])) {
     echo "<h1>jersey nash printed</h1><br>";
-}else{
-    echo "not in cart";
 }
 
 
 if(isset( $_SESSION["jerseywade"])) {
     echo "<h1>jersey wade printed</h1><br>";
-}else{
-    echo "not in cart";
 }
 
 
 if(isset( $_SESSION["jerseywestbrook"])) {
     echo "<h1>jersey westbrook printed</h1><br>";
-}else{
-    echo "not in cart";
 }
-
 
 ?>
