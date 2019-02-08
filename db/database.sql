d642fb174f923509a122fba75782ff0432fc2dce
david aruldass <davidaruldass@yahoo.com>
2019-02-08 10:54:40 -0700

    added php file

--- db/database.sql
+++ db/database.sql
@@ -2,6 +2,7 @@
 -- to create a table -- copy and paste the table 
 -- \i database.sql to drop and create the tables 
 -- heroku pg:psql -- connect to heroku database
+-- to view table just type ;
 
 
 DROP TABLE appointment;
@@ -13,14 +14,13 @@ DROP TYPE servicetype;
 
 CREATE TABLE customer(
     customer_id                  SERIAL PRIMARY KEY,  -- default 
-    customer_firstname           varchar(100),   -- need to preset the maximum amount of character 
-    customer_lastname            varchar(100),
-    customer_phonenumber         int, 
-    customer_email               varchar(100),
-    customer_registerdate        date,          -- just gets the date LOOK UP HOW TO UPDATE A SINGLE TABLE 
+    customer_firstname           varchar(100) NOT NULL,   -- need to preset the maximum amount of character 
+    customer_lastname            varchar(100) NOT NULL,
+    customer_phonenumber         VARCHAR NOT NULL, 
+    customer_email               varchar(100) NOT NULL,
+    customer_registerdate        date NOT NULL,          -- just gets the date LOOK UP HOW TO UPDATE A SINGLE TABLE 
     customer_gender              varchar(5)
 );
-
 
 CREATE TYPE homesize AS ENUM ('small', 'medium', 'large');
 CREATE TYPE servicetype AS ENUM ('interior', 'exterior', 'both');
@@ -35,12 +35,34 @@ CREATE TABLE service_type(
     service_Price               smallserial  --https://www.postgresql.org/docs/9.3/datatype-numeric.html
 );
 
-
 CREATE TABLE appointment(
     appointment_id                  SERIAL PRIMARY KEY,
     appointment_date                timestamp, -- this will get the date and time           
     appointment_address             varchar, -- with numbers if you don't do math with them, they are consider charactor 
-    customer_id                     SERIAL REFERENCES customer(customer_id), -- to connect foreign key 
-    service_id                      SERIAL REFERENCES service_type(service_id)
+    customer_id                     INT REFERENCES customer(customer_id), -- to connect foreign key 
+    service_id                      INT REFERENCES service_type(service_id)
 );
 
+
+-- VALUES (1, 2, 3)
+-- research on how SERIAL WORKS AND HOW I CAN INSERT INTO TABLE WITH SERIAL DATATYPE
+
+INSERT INTO customer
+values (default, 'John', 'Smith', '8016596598', 'johnsmith@gmail.com', '12-25-2019', NULL),
+(default, 'David', 'Aruldass', '8016596551', 'davidaruldass@gmail.com', '12-26-2019', NULL),
+(default, 'Daniel', 'Aruldass', '8016596552', 'danielaruldass@gmail.com', '12-27-2019', NULL);
+
+INSERT INTO service_type VALUES 
+(DEFAULT, 'small', 'interior', 250, 'grey', '10:00am', '3000'),
+(DEFAULT, 'medium', 'exterior', 500, 'grey', '10:00am', '4000'),
+(DEFAULT, 'large', 'both', 700, 'grey', '10:00am', '5000');
+
+INSERT INTO appointment VALUES 
+(DEFAULT,'12-28-2019', '123 Walter Street #2121 Provo, Utah 84604', 1 , 1),
+(DEFAULT,'12-28-2019', '124 Walter Street #2121 Provo, Utah 84604' , 2, 2),
+(DEFAULT,'12-28-2019', '125 Walter Street #2121 Provo, Utah 84604' ,3 , 3);
+
+
+
+--insert into customer (customer_firstname, customer_lastname...)
+--values ('john', 'smith')
