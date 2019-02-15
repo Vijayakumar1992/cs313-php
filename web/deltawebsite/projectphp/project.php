8ea0236176d52f9bde01940aaef60825c8cababd
david aruldass <davidaruldass@yahoo.com>
2019-02-15 16:32:32 -0700

    adfd

--- web/deltawebsite/projectphp/project.php
+++ web/deltawebsite/projectphp/project.php
@@ -56,6 +56,8 @@ $statement->execute();
 $resultSet = $statement->fetch(); // get the first result of the query above
 $statement->closeCursor();// closes the interaction with the database
 //this would get the result from each column and displays it as rows 
+var_dump($resultSet); // takes all the info in array and dumps it out into the page
+exit;
 echo $resultSet["customer_id"] .$resultSet["customer_firstname"].$resultSet["customer_lastname"].$resultSet["customer_email"];
 
 
